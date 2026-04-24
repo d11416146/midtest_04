5be5208178e0c606a75e0510c3081b0cc809d9f7
d11416146 <d11416146@gs.takming.edu.tw>
2026-04-24 13:36:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace midtest_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Console.WriteLine(p1.name);
+
+            Person p2 = new Person("New Person");
+            Console.WriteLine(p2.name);
+
+            Student s1= new Student();
+            Console.WriteLine(s1.name);
+
+            Student s2 = new Student("New Student");
+            Console.WriteLine(s2.name);
+        }
+    }
+    public  class Person
+    {
+        public string name;
+
+        public Person()
+        {
+            name = "Person";
+        }
+        public Person(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public class Student:Person
+    {
+        public Student():base()
+        {
+
+        }
+
+        public Student(string name): base(name) 
+        {
+
+        }
+    }
+}
